2898fafd39559d8cd6987bfd034784b809656085
Ezra Thomas <46074998+ept221@users.noreply.github.com>
2019-07-22 12:30:07 +0300

    Update pose_estimate_patcher.py

--- src/pose_estimate_patcher.py
+++ src/pose_estimate_patcher.py
@@ -2,9 +2,13 @@
 
 # This node subscribes to a topic published by rviz which gives a pose estimate for a robot
 # and corrects its frame_id so that it will properly work in a multi-robot system.
-# Specifically, it changes the frame_id from "map" to "/map" 
+# In a regular single robot system, rviz publishes pose estimates to /initialpose. In 
+# the multibot system, rviz is configured to publish into specific namespaces, eg.
+# /robot1/initialpose/patch and /robot2/initialpose/patch. This node lives inside one of the
+# robot namespaces, and subscribes to initialpose/patch. It then corrects the message
+# published by rviz by changing the frame_id from "map" to "/map" and then publishes this modified
+# message to initialpose/ where the bot's navigation stack is waiting for it.
 
-import sys
 import rospy
 from geometry_msgs.msg import PoseWithCovarianceStamped
 
@@ -14,8 +18,11 @@ def callback(msg):
   print msg
 
 
-sub_topic = "/" + sys.argv[1] + "/initialpose/patch"
-pub_topic = "/" + sys.argv[1] + "/initialpose/"
+#sub_topic = "/" + sys.argv[1] + "initialpose/patch"
+#pub_topic = "/" + sys.argv[1] + "initialpose/"
+
+sub_topic = "initialpose/patch"
+pub_topic = "initialpose/"
 
 rospy.init_node("pose_estimate_patcher")
 sub = rospy.Subscriber(sub_topic, PoseWithCovarianceStamped, callback)
